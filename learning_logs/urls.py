cc25cf654b861f15227967b166eff174f59cb895
isakez <isaksanchez21@gmail.com>
2025-10-10 08:21:24 +0200

    pagina de los topics con sus entradas

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     # Home page
     path('', views.index, name='index'),
     # Page that shows all topics.
-    path('topics/', views.topics, name = 'topics')
+    path('topics/', views.topics, name = 'topics'),
+    path('topics/<int:topic_id>/'views.topic, name='topic'),
 ]
