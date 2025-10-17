bf0ff87dee8441e6af874254546524139ca31870
isakez <isaksanchez21@gmail.com>
2025-10-17 07:14:41 +0200

    login required y logout

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps
     'learning_logs',
+    'users',
     
     # Default apps
     'django.contrib.admin',
@@ -124,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#My settings
+LOGIN_URL = 'users:login'
+LOGOUT_REDIRECT_URL = "/"
